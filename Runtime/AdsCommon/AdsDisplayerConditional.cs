863fb667566a3eb2a2360eb220984945697d304d
mnicolas94 <miguelcok27@gmail.com>
2024-03-09 15:03:22 -0500

    chore(ads): -

--- Runtime/AdsCommon/AdsDisplayerConditional.cs
+++ Runtime/AdsCommon/AdsDisplayerConditional.cs
@@ -36,6 +36,7 @@ namespace ServicesUtils.AdsCommon
             if (timer.ShouldShow())
             {
                 await DisplayAdAsync(destroyCancellationToken);
+                timer.UpdateTimer();
             }
         }
         
