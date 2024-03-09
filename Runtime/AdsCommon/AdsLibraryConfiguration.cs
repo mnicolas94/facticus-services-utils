76866303660e896b50c923df241682be65a380d9
mnicolas94 <miguelcok27@gmail.com>
2024-03-08 21:00:13 -0500

    feat(ads commons): implement ads library configuration

---
+++ Runtime/AdsCommon/AdsLibraryConfiguration.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+namespace ServicesUtils.AdsCommon
+{
+    [CreateAssetMenu(fileName = "AdsLibraryConfiguration", menuName = "ServicesUtils/Ads/AdsLibrary/LibraryConfig", order = 0)]
+    public class AdsLibraryConfiguration : ScriptableObject
+    {
+        [SerializeField] private AdsLibraryEnum _libraryToUse;
+
+        public bool IsLibrary(AdsLibraryEnum library)
+        {
+            return _libraryToUse == library;
+        }
+    }
+}
---
+++ Runtime/AdsCommon/AdsLibraryEnum.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace ServicesUtils.AdsCommon
+{
+    [CreateAssetMenu(fileName = "AdsLibraryEnum", menuName = "ServicesUtils/Ads/AdsLibrary/LibraryEnum", order = 0)]
+    public class AdsLibraryEnum : ScriptableObject
+    {
+        
+    }
+}
