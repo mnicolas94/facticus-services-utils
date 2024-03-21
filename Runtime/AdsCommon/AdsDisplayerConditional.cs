6a39fa717d9f414f80db08c023aac2a3cb6a66e1
mnicolas94 <miguelcok27@gmail.com>
2024-03-21 18:16:03 -0400

    fix(ads common): remove using statement

--- Runtime/AdsCommon/AdsDisplayerConditional.cs
+++ Runtime/AdsCommon/AdsDisplayerConditional.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.Threading;
 using System.Threading.Tasks;
 using SerializableCallback;
-using ServicesUtils.AdsLegacy;
 using TNRD;
 using UnityEngine;
 
